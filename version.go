0a46cd54f1f99247c5b4b2ee3cd8c3ef256b56af
nekrassov01 <unclek62@gmail.com>
2026-04-07 00:44:29 +0900

    refactor: versioning

---
+++ version.go
@@ -0,0 +1,17 @@
+package s3bytes
+
+import "fmt"
+
+// version is the current version.
+const version = "0.0.18"
+
+// revision is the git revision.
+var revision = ""
+
+// Version returns the version and revision.
+func Version() string {
+	if revision == "" {
+		return version
+	}
+	return fmt.Sprintf("%s (revision: %s)", version, revision)
+}
---
+++ version_test.go
@@ -0,0 +1,35 @@
+package s3bytes
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestVersion(t *testing.T) {
+	tests := []struct {
+		name     string
+		version  string
+		revision string
+		want     string
+	}{
+		{
+			name:     "basic",
+			revision: "1234567",
+			want:     fmt.Sprintf("%s (revision: 1234567)", version),
+		},
+		{
+			name:     "no revision",
+			version:  version,
+			revision: "",
+			want:     version,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			revision = tt.revision
+			if got := Version(); got != tt.want {
+				t.Errorf("version() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
